993c684d104875d03b33423deec7d7eac14d6bac
seokmin <devhong0016@gmail.com>
2025-04-13 00:29:50 +0900

    feat: 로깅 추가 winston

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json()
+          ),
+        }),
+      ],
+    }),
+  });
   
   app.enableCors({
     origin: `${process.env.FRONTEND_URL}`,
@@ -12,6 +25,8 @@ async function bootstrap() {
   
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   
   await app.listen(process.env.PORT || 3000);
 }
